d9a0f18cb96b6f65989ed52d468ea05c5d227303
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-06-07 15:41:48 -0400

    WIP - Resultifying aggregator. Also defining finite states

--- Baubit.Aggregation/AEventAggregator.cs
+++ Baubit.Aggregation/AEventAggregator.cs
@@ -5,9 +5,21 @@ using Baubit.IO.Channels;
 using Baubit.Tasks;
 using Baubit.Aggregation.ResultReasons;
 using Baubit.Traceability;
+using FluentResults.Extensions;
+using System.Collections.Concurrent;
 
 namespace Baubit.Aggregation
 {
+    public enum AggregatorState
+    {
+        None,
+        NotStarted,
+        Running,
+        Faulted,
+        Stopped,
+        Disposing,
+        Disposed
+    }
     public abstract class AEventAggregator<TEvent> : IEventAggregator<TEvent>
     {
         private IList<AEventDispatcher<TEvent>> _dispatchers = new ConcurrentList<AEventDispatcher<TEvent>>();
@@ -15,6 +27,20 @@ namespace Baubit.Aggregation
         protected CancellationTokenSource aggregationCancellationTokenSource;
         protected Task _distributor;
         private readonly DispatcherFactory<TEvent> _dispatcherFactory;
+
+        private ConcurrentStack<AggregatorState> aggregatorStates = new ConcurrentStack<AggregatorState>([AggregatorState.NotStarted]);
+
+        public AggregatorState State 
+        {
+            get
+            {
+                return aggregatorStates.TryPeek(out var currentState) ? currentState : AggregatorState.None;
+            }
+            private set
+            {
+                aggregatorStates.Push(value);
+            }
+        }
 
         protected AEventAggregator(AggregatorConfiguration aggregatorConfiguration,
                                    Channel<TEvent> channel,
@@ -28,39 +54,43 @@ namespace Baubit.Aggregation
             }
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            if (_distributor != null) return Task.CompletedTask;
-            aggregationCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-            Parallel.ForEach(_dispatchers, async dispatcher => await dispatcher.StartAsync(aggregationCancellationTokenSource.Token));
-            _distributor = Task.Run(() => _channel.ReadAsync(OnNextEvent, aggregationCancellationTokenSource.Token));
-            return Task.CompletedTask;
+            (await StartAsyncInternal(cancellationToken)).ThrowIfFailed();
         }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+        private async Task<Result> StartAsyncInternal(CancellationToken cancellationToken)
         {
-            aggregationCancellationTokenSource.Cancel();
-            _distributor?.Wait(true);
-            Parallel.ForEach(_dispatchers, async dispatcher => await dispatcher.StopAsync(cancellationToken));
-            _distributor = null;
-            return Task.CompletedTask;
+            return await Result.OkIf(_distributor == null, string.Empty)
+                               .Bind(() => Result.Try(() => aggregationCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken)))
+                               .Bind(_ => Result.Try(() => Parallel.ForEach(_dispatchers, async dispatcher => await dispatcher.StartAsync(aggregationCancellationTokenSource.Token))))
+                               .Bind(parallelLoopResult => Result.OkIf(parallelLoopResult.IsCompleted, string.Empty))
+                               .Bind(() => Result.Try(() => _distributor = Task.Run(() => _channel.ReadAsync(OnNextEvent, aggregationCancellationTokenSource.Token))))
+                               .Bind(() => Result.Try(() => State = AggregatorState.Running).Bind(_ => Task.FromResult(Result.Ok())));
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            (await StopAsyncInternal(cancellationToken)).ThrowIfFailed();
+        }
+
+        private async Task<Result> StopAsyncInternal(CancellationToken cancellationToken)
+        {
+            return await Result.Try(() => aggregationCancellationTokenSource.Cancel())
+                               .Bind(() => _distributor?.Wait(true))
+                               .Bind(() => Result.Try(() => Parallel.ForEach(_dispatchers, async dispatcher => await dispatcher.StopAsync(cancellationToken))))
+                               .Bind(parallelLoopResult => Result.OkIf(parallelLoopResult.IsCompleted, string.Empty))
+                               .Bind(() => Result.Try(() => _distributor = null))
+                               .Bind(() => Result.Try(() => State = AggregatorState.Stopped).Bind(_ => Task.FromResult(Result.Ok())));
         }
 
         public async Task<Result<IDisposable>> TrySubscribeAsync(IObserver<TEvent> observer)
         {
-            await Task.Yield();
-            try
-            {
-                if (aggregationCancellationTokenSource != null && aggregationCancellationTokenSource.IsCancellationRequested) return Result.Fail("").WithReason(new AggregatorDisposed());
-                var dispatcher = _dispatcherFactory.Invoke(observer, _dispatchers);
-                await dispatcher.StartAsync(aggregationCancellationTokenSource?.Token ?? CancellationToken.None);
-                _dispatchers.Add(dispatcher);
-                return Result.Ok<IDisposable>(dispatcher);
-            }
-            catch (Exception exp)
-            {
-                return Result.Fail(new ExceptionalError(exp));
-            }
+            return await Result.FailIf(aggregationCancellationTokenSource?.IsCancellationRequested == true, string.Empty)
+                         .AddReasonIfFailed(new AggregatorDisposed())
+                         .Bind(() => Result.Try(() => _dispatcherFactory.Invoke(observer, _dispatchers)))
+                         .Bind(dispatcher => Result.Try(() => dispatcher.StartAsync(aggregationCancellationTokenSource?.Token ?? CancellationToken.None))
+                                                   .Bind(() => Task.FromResult(Result.Try(() => _dispatchers.Add(dispatcher)).Bind(() => Result.Ok<IDisposable>(dispatcher)))));
         }
 
         public async virtual Task<Result> TryPublishAsync(TEvent @event, CancellationToken cancellationToken = default, TimeSpan? maxWaitToWrite = null)
@@ -70,10 +100,7 @@ namespace Baubit.Aggregation
 
         private async Task OnNextEvent(TEvent @event, CancellationToken cancellationToken)
         {
-            foreach (var dispatcher in _dispatchers)
-            {
-                var dispatchResult = await dispatcher.TryDispatchAsync(@event, cancellationToken);
-            }
+            await _dispatchers.Aggregate(Task.FromResult(Result.Ok()), (seed, next) => seed.Bind(() => next.TryDispatchAsync(@event, cancellationToken)));
         }
 
         protected virtual void Dispose(bool disposing)
@@ -81,12 +108,14 @@ namespace Baubit.Aggregation
             aggregationCancellationTokenSource.Cancel();
             if (disposing)
             {
+                State = AggregatorState.Disposing;
                 _distributor?.Wait(true);
                 _distributor = null;
                 _channel?.FlushAndDispose();
                 _channel = null;
                 _dispatchers?.Dispose();
                 _dispatchers = null;
+                State = AggregatorState.Disposed;
             }
         }
 
